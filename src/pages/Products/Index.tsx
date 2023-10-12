f9b4f0d6252a93a3450dbc20e9161c710c2eb94c
douglasgalvao <douglas-mad@live.com>
2023-10-12 06:17:41 -0300

    feat create page Products

---
+++ src/pages/Products/Index.tsx
@@ -0,0 +1,8 @@
+import { Navbar } from "../../components/Navbar/Index"
+import { Screen } from "../../components/Screen/Index"
+export const ProductsPage = () => {
+    return <div style={{width:'100vw',height:'100vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
+        <Navbar />
+        <Screen width="80%" height="80%" backgroundColor="#1e90ff" radius="15px" titleScreen="Products" titleColor="white"/>
+    </div>
+}
