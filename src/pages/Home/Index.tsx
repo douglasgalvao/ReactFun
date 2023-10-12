45675a65dedeca34ac6eca68c170005a513278dd
douglasgalvao <douglas-mad@live.com>
2023-10-12 06:17:49 -0300

    fix page Home

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,10 +1,8 @@
 import { Navbar } from "../../components/Navbar/Index"
-
+import { Screen } from "../../components/Screen/Index"
 export const HomePage = () => {
     return <div style={{width:'100vw',height:'100vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
         <Navbar />
-        <div style={{width:'80%',height:'80%',backgroundColor:'white',borderRadius:'25px'}}>
-            
-        </div>
+        <Screen width="80%" height="80%" backgroundColor="#1e90ff" radius="15px" titleScreen="Home" titleColor="white"/>
     </div>
 }
